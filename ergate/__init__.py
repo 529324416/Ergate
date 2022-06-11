813d9849cef56125aeedc02faf733644ec947adc
PrinceBiscuit <30776995+529324416@users.noreply.github.com>
2022-06-12 06:29:23 +0800

    uploading
    ...

---
+++ ergate/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/python3
+# load all components in ergate
+# time: 2020.08.05
+
+from ergate.core import *
+from ergate.utils import *
+from ergate.spider import *
+
+__version__ = '1.1'
+__author__ = 'biscuit'
